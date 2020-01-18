d01573742f18f745066c81e206fa4978cebb7945
exizt <e2xist@gmail.com>
2020-01-18 20:40:12 +0900

    1.33

--- mediawiki_installer.sh
+++ mediawiki_installer.sh
@@ -1,93 +1,73 @@
 #! /bin/bash
 # ####################################
-# MediaWiki Installer Using Git
+# GIT 을 이용해서 Mediawiki 설치하기
 # @Version : 1.3.190123
 # @git : https://github.com/exizt/mediawiki_installer_script
 # 
-# 미디어위키를 Git 으로 설치하는 스크립트 입니다. 스크립트를 동작시키는 폴더 하위에 생성합니다.
+# 리눅스 용으로 만들었는데... 윈도우에서도 동작이 되네요.
 # ####################################
-
-# ####################################
-# Configurations
-
-# git branch name for git cloning
-CF_MW_BRANCH="REL1_32"
-
-# directory name to be created
-CF_MW_DIRNAME="wiki_1_32"
-
-# mediawiki extensions
-CF_MW_EXTENSIONS=("CharInsert" "Cite" "CiteThisPage" "ConfirmEdit" "Gadgets" "ImageMap" "InputBox" "Interwiki" "LocalisationUpdate" "Nuke" "ParserFunctions" "Poem" "Renameuser" "SpamBlacklist" "SyntaxHighlight_GeSHi" "TitleBlacklist" "WikiEditor" )
-
-
+# 
+# [Configurations]
+#
+# 생성할 폴더명
+CF_MW_DIRNAME="wiki_1_33"
+# 
+# 브랜치. 보통 REL1_33 과 같은 식임.
+CF_MW_BRANCH="REL1_33"
+# 
+# 확장 기능 들.
+CF_MW_EXTENSIONS=("CharInsert" "Cite" "CiteThisPage" "ConfirmEdit" "Gadgets" "ImageMap" "InputBox" "Interwiki" "LocalisationUpdate" "Nuke" "ParserFunctions" "Poem" "Renameuser" "SpamBlacklist" "SyntaxHighlight_GeSHi" "TitleBlacklist" "WikiEditor" "Math" "Kartographer" "CodeEditor")
+# 
 # ####################################
 # Variables
 script_dir=$(cd "$(dirname "$0")" && pwd)
 cd $script_dir
 
 wikiName="$CF_MW_DIRNAME"
-wikiPath="$script_dir/$wikiName"
+wikiPath="${script_dir}/${wikiName}"
 
 
 # ####################################
-# ## 1. make mediawiki core ## 
+# 처리 부분
 
 
-# 1.(1) command for git clone
-# clone git and create wiki directories
-cmd_cr_core_git_clone="git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git $wikiName"
+# 1. 미디어위키 코어 git cloning
+cmd_clone_core="git clone https://gerrit.wikimedia.org/r/mediawiki/core.git --branch ${CF_MW_BRANCH} --depth=1 $wikiName"
 
+# composer 셋팅, images 폴더 퍼미션 등
+cmd_clone_core_step2="cd ${wikiPath} && composer update --no-dev && chown -R apache:apache images/"
 
-# 1.(2) command for checkout git
-# 원하는 버전대로 맞춰두어야 나중에 수월하다.
-cmd_cr_core_checkout="cd $wikiName && git checkout -b ${CF_MW_BRANCH} origin/${CF_MW_BRANCH}"
-
-
-# 1.(3) command for composer update and image folder permission
-# composer update 를 완료하면 vendor 폴더가 생성이 된다.
-# create vender folder after composer update
-cmd_cr_core_after="composer update --no-dev && chown -R apache:apache images/"
-
-# 1.E. 명령어 실행 과 composer update 동작
-# execute commands (1) (2) (3)
-eval "$cmd_cr_core_git_clone && $cmd_cr_core_checkout && $cmd_cr_core_after"
-
-echo "has completed git cloning"
+# 명령어 실행부
+eval "${cmd_clone_core} && ${cmd_clone_core_step2}"
+echo "step 1/3 미디어위키 코어 복사 완료."
 sleep 0.5
 
 
-# ## 2. make mediawiki skins ## 
-
-
-# 2번과 3번을 사용 안 하고, 한번에 스킨과 확장기능 전부를 한번에 설치하려면. 아래의 명령어를 사용한다.
+# 바로 submodule 로 처리하고 싶을 때.
 # git submodule update --init --recursive
 
 
-# 2. download and clone 'mediawiki skins'
-# skins 폴더 하위 내용 생성. 나는 Vector 만 사용할 것이라서, Vector 만 생성함.
-cd $wikiPath"/skins"
-git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector Vector && cd Vector && git checkout ${CF_MW_BRANCH}
+# 스킨 git cloning
+cmd_clone_skins="cd ${wikiPath}/skins && git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector --branch ${CF_MW_BRANCH} --depth=1 Vector"
+eval $cmd_clone_skins
+echo "step 2/3 미디어위키 스킨 복사 완료."
+sleep 0.5
 
 
-
-# ## 3. make mediawiki extensions ## 
-
-
-# 3.1 download and clone 'media wiki extetions' from git
+# 확장 기능들
 for extension_name in "${CF_MW_EXTENSIONS[@]}"; do
-	echo $value
-	cd $wikiPath"/extensions"
-	command="git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/${extension_name}.git && cd ${extension_name} && git checkout ${CF_MW_BRANCH}"
-	# echo $command
-	eval $command
+# command="git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/${extension_name}.git && cd ${extension_name} && git checkout ${CF_MW_BRANCH}"
+	cmd_clone_extensions="cd ${wikiPath}/extensions && git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/${extension_name}.git --branch ${CF_MW_BRANCH} --depth=1 ${extension_name}"
+	echo $cmd_clone_extensions
+	eval $cmd_clone_extensions
 	sleep 0.5
 done
 
 # 3.2 download and clone other extensions
-cd $wikiPath"/extensions"
+# cd $wikiPath"/extensions"
 # git clone https://github.com/HydraWiki/mediawiki-embedvideo.git EmbedVideo
-git clone https://github.com/wikimedia/mediawiki-extensions-Kartographer.git Kartographer
-
+# git clone https://github.com/wikimedia/mediawiki-extensions-Kartographer.git Kartographer
+# git clone https://gerrit.wikimedia.org/g/mediawiki/extensions/Kartographer Kartographer
 
 # 4. fin
 cd $wikiPath
